71a9628430b52c167575fb2a5d24606112320057
AbhyasKanaujia <k.abhyas@gmail.com>
2025-06-08 14:32:31 +0530

    Hello, World

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App" style={{fontFamily: "sans-serif", padding: "2rem", }}>
+      <h1>Drain & Charge Tracker</h1>
+      <p>Hello from the client!</p>
+      <a target="_blank" href="/api/hello">Say hello to the server</a>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/src/index.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const express = require("express");
+
+const connectDB = require("./config/db.js");
+
+const helloRouter = require("./routes/hello.js");
+
+dotenv.config();
+const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(cors());
+  console.log("cors enabled");
+}
+app.use(express.json());
+connectDB();
+
+app.use("/api/hello", helloRouter);
+
+if (process.env.NODE_ENV === "production") {
+  const clientBuildPath = path.join(__dirname, "../../client/build");
+  app.use(express.static(clientBuildPath));
+
+  app.get(":splat*", (req, res) => {
+    res.sendFile(path.resolve(clientBuildPath, "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5050;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
